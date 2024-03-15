53381c07a5864c8829407c01b2d73d7fb99c1f22
OscarFerBarrera <osjamfer@hotmail.com>
2024-03-15 23:37:36 +0100

    Ejercicio terminado, funciona get y post

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const PORT = 3000;
 const server = express();
 const router = express.Router();
-const pilotosF1 = "./docs/pilotos.json";
+const pilotosFilePath = "./docs/pilotos.json";
 
 // Configuración del server
 server.use(express.json());
@@ -22,7 +22,7 @@ router.get("/", (req, res) => {
 });
 
 router.get("/f1-driver", (req, res) => {
-  fs.readFile(pilotosF1, (error, data) => {
+  fs.readFile(pilotosFilePath, (error, data) => {
     if (error) {
       res.status(500).send("Error inesperado");
     } else {
@@ -32,47 +32,47 @@ router.get("/f1-driver", (req, res) => {
   });
 });
 
-// router.post("/pokemon", (req, res) => {
-//   // Leemos el fichero pokemons
-//   fs.readFile(pilotosF1, (error, data) => {
-//     if (error) {
-//       res.status(500).send("Error inesperado");
-//     } else {
-//       const pokemons = JSON.parse(data);
-//       const newPokemon = req.body;
-//       const lastId = pokemons[pokemons.length - 1].id;
-//       newPokemon.id = lastId + 1;
-//       pokemons.push(newPokemon);
+router.post("/f1-driver", (req, res) => {
+  // Leemos el fichero pokemons
+  fs.readFile(pilotosFilePath, (error, data) => {
+    if (error) {
+      res.status(500).send("Error inesperado");
+    } else {
+      const pilotosF1 = JSON.parse(data);
+      const newPilotoF1 = req.body;
+      const lastId = pilotosF1[pilotosF1.length - 1].id;
+      newPilotoF1.id = lastId + 1;
+      pilotosF1.push(newPilotoF1);
 
-//       // Guardamos fichero
-//       fs.writeFile(pilotosF1, JSON.stringify(pokemons), (error) => {
-//         if (error) {
-//           res.status(500).send("Error inesperado");
-//         } else {
-//           res.json(newPokemon);
-//         }
-//       });
-//     }
-//   });
-// });
+      // Guardamos fichero
+      fs.writeFile(pilotosFilePath, JSON.stringify(pilotosF1), (error) => {
+        if (error) {
+          res.status(500).send("Error inesperado");
+        } else {
+          res.json(newPilotoF1);
+        }
+      });
+    }
+  });
+});
 
-// router.get("/pokemon/:id", (req, res) => {
-//   fs.readFile(pilotosF1, (error, data) => {
-//     if (error) {
-//       res.status(500).send("Error inesperado");
-//     } else {
-//       const id = parseInt(req.params.id);
-//       const pokemons = JSON.parse(data);
-//       const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+router.get("/f1-driver/:id", (req, res) => {
+  fs.readFile(pilotosFilePath, (error, data) => {
+    if (error) {
+      res.status(500).send("Error inesperado");
+    } else {
+      const id = parseInt(req.params.id);
+      const pilotos = JSON.parse(data);
+      const piloto = pilotos.find((piloto) => piloto.id === id);
 
-//       if (pokemon) {
-//         res.json(pokemon);
-//       } else {
-//         res.status(404).send("Pokemon con encontrado.");
-//       }
-//     }
-//   });
-// });
+      if (piloto) {
+        res.json(piloto);
+      } else {
+        res.status(404).send("Piloto no encontrado.");
+      }
+    }
+  });
+});
 
 server.use("/", router);
 
